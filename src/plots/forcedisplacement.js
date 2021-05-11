0becbe77b3e93d9c61771655c618e4dd8471f4c8
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2021-05-11 10:16:57 +1000

    error bar checkbox

--- src/plots/forcedisplacement.js
+++ src/plots/forcedisplacement.js
@@ -1,32 +1,42 @@
 import { defaultValues, data, colors } from "./commons";
 import Plot from "react-plotly.js";
+import { useState } from "react";
 
 export default function ForceDisplacement() {
+  const [errorEnabled, setErrorEnabled] = useState(false);
   return (
-    <Plot
-      {...defaultValues("Force Displacement")}
-      layout={{
-        xaxis: {
-          title: "Displacement (mm)",
-        },
-        yaxis: {
-          title: "Force (kN)",
-        },
-        title: "Force / Displacement",
-      }}
-      data={data.map((line, index) => ({
-        x: line.displacement,
-        y: line.force,
-        mode: "lines",
-        name: line.name,
-        line: {
-          color: colors[index],
-        },
-        error_y: {
-          type: "constant",
-          value: 0.0005,
-        },
-      }))}
-    />
+    <>
+      <input
+        type="checkbox"
+        value={errorEnabled}
+        onChange={(e) => setErrorEnabled(e.target.checked)}
+      />
+      <Plot
+        {...defaultValues("Force Displacement")}
+        layout={{
+          xaxis: {
+            title: "Displacement (mm)",
+          },
+          yaxis: {
+            title: "Force (kN)",
+          },
+          title: "Force / Displacement",
+        }}
+        data={data.map((line, index) => ({
+          x: line.displacement,
+          y: line.force,
+          mode: "lines",
+          name: line.name,
+          line: {
+            color: colors[index],
+          },
+          error_y: {
+            type: "constant",
+            value: 0.0005,
+            visible: errorEnabled,
+          },
+        }))}
+      />
+    </>
   );
 }
--- src/plots/forcetime.js
+++ src/plots/forcetime.js
@@ -1,32 +1,42 @@
 import { defaultValues, data, colors } from "./commons";
 import Plot from "react-plotly.js";
+import { useState } from "react";
 
 export default function ForceTime() {
+  const [errorEnabled, setErrorEnabled] = useState(false);
   return (
-    <Plot
-      {...defaultValues("Force Time")}
-      layout={{
-        xaxis: {
-          title: "Time (s)",
-        },
-        yaxis: {
-          title: "Force (kN)",
-        },
-        title: "Force / Time",
-      }}
-      data={data.map((line, index) => ({
-        x: line.time,
-        y: line.force,
-        mode: "lines",
-        name: line.name,
-        error_y: {
-          type: "constant",
-          value: 0.0005,
-        },
-        line: {
-          color: colors[index],
-        },
-      }))}
-    />
+    <>
+      <input
+        type="checkbox"
+        value={errorEnabled}
+        onChange={(e) => setErrorEnabled(e.target.checked)}
+      />
+      <Plot
+        {...defaultValues("Force Time")}
+        layout={{
+          xaxis: {
+            title: "Time (s)",
+          },
+          yaxis: {
+            title: "Force (kN)",
+          },
+          title: "Force / Time",
+        }}
+        data={data.map((line, index) => ({
+          x: line.time,
+          y: line.force,
+          mode: "lines",
+          name: line.name,
+          error_y: {
+            type: "constant",
+            value: 0.0005,
+            visible: errorEnabled,
+          },
+          line: {
+            color: colors[index],
+          },
+        }))}
+      />
+    </>
   );
 }
